580b361b5198d4176761de1da73858a76255e29a
Ruslan <55135710+RuLeon@users.noreply.github.com>
2021-11-07 13:48:12 +0300

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,30 +1,39 @@
-package Razdel4.HomeWork;
+package Razdel5.HomeWork;
 
 /**
- * В переменной minutes лежит число от 0 до 59.
- * Написать функцию, которая принимает в качестве параметра значение переменной minutes
- * и выводит на консоль в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
- * Протестировать функцию в main.
+ * Посчитать четные и нечетные цифры целого числа и вывести их на консоль.
+ * Для решения написать 2 функции, которые будут принимать введенное целое число,
+ * а возвращать количество четных цифр (вторая функция - нечетных).
+ * Например: если введено число 228910, то у него 4 четные цифры (2, 2, 8, 0) и 2 нечетные (9, 1).
  */
 
 public class Task1 {
     public static void main(String[] args) {
-        int minutes = 0;
-        quarterOfAnHour(minutes);
+        int value = 228910;
+        int counter1 = quantityOfChetnie(value);
+        int counter2 = quantityOfNeChetnie(value);
+        System.out.println("В введенном числе " + counter1 + " ЧЕТНЫХ и " + counter2 + " НЕЧЕТНЫХ цифры.");
     }
 
-    public static void quarterOfAnHour(int minutes) {
-        if (minutes >= 0 && minutes <= 15) {
-            System.out.println(minutes + "-ая минута попадает в первую четверть часа.");
-        } else if (minutes > 15 && minutes <= 30) {
-            System.out.println(minutes + "-ая минута попадает во вторую четверть часа.");
-        } else if (minutes > 31 && minutes <= 45) {
-            System.out.println(minutes + "-ая минута попадает в третью четверть часа.");
-        } else if (minutes > 45 && minutes <= 59) {
-            System.out.println(minutes + "-ая минута попадает в третью четверть часа.");
-        } else {
-            System.out.println("Невозможно определить в какую четверть часа попадает указанная минута, " +
-                    "т.к. в одном часе 60 минут!");
+    public static int quantityOfChetnie(int value) {
+        int counter = 0;
+        for (int currentValue = value; currentValue != 0; currentValue = currentValue / 10) {
+            int number = currentValue % 10;
+            if (number % 2 == 0) {
+                counter++;
+            }
         }
+        return counter;
+    }
+
+    public static int quantityOfNeChetnie(int value) {
+        int counter = 0;
+        for (int currentValue = value; currentValue != 0; currentValue = currentValue / 10) {
+            int number = currentValue % 10;
+            if (number % 2 != 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
