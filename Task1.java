7d0a95ab9b4038db197f20e614163c3f6f805d09
Ruslan <55135710+RuLeon@users.noreply.github.com>
2021-11-14 20:13:10 +0200

    Arrays

--- Task1.java
+++ Task1.java
@@ -1,30 +1,29 @@
-package Razdel4.HomeWork;
+package razdel6.homework;
 
 /**
- * В переменной minutes лежит число от 0 до 59.
- * Написать функцию, которая принимает в качестве параметра значение переменной minutes
- * и выводит на консоль в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
- * Протестировать функцию в main.
+ * Дан одномерный массив целых чисел.
+ * Написать функцию, удаляющую из него все отрицательные элементы
+ * (удалить - значит создать новый массив с только положительными элементами).
+ * После удаления - умножить каждый элемент массива на его длину.
+ * Например: [3, 5, -6, 3, 2, -9, 0, -123] -> [15, 25, 15, 10, 0]
+ * Не забывать, что всю логику приложения нужно выносить в отдельные функции.
+ * main - только для тестирования написанного функционала
  */
-
 public class Task1 {
+
     public static void main(String[] args) {
-        int minutes = 0;
-        quarterOfAnHour(minutes);
+        int[] values = {3, 5, -6, 3, 2, -9, 0, -123};
+        int[] resultArray = deleteNegativeElements(values);
     }
 
-    public static void quarterOfAnHour(int minutes) {
-        if (minutes >= 0 && minutes <= 15) {
-            System.out.println(minutes + "-ая минута попадает в первую четверть часа.");
-        } else if (minutes > 15 && minutes <= 30) {
-            System.out.println(minutes + "-ая минута попадает во вторую четверть часа.");
-        } else if (minutes > 31 && minutes <= 45) {
-            System.out.println(minutes + "-ая минута попадает в третью четверть часа.");
-        } else if (minutes > 45 && minutes <= 59) {
-            System.out.println(minutes + "-ая минута попадает в третью четверть часа.");
-        } else {
-            System.out.println("Невозможно определить в какую четверть часа попадает указанная минута, " +
-                    "т.к. в одном часе 60 минут!");
+    public static int[] deleteNegativeElements(int[] values) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] >= 0) {
+                values[i] *= 5;
+                System.out.print(values[i] + " ");
+            }
         }
+        return values;
     }
 }
+
